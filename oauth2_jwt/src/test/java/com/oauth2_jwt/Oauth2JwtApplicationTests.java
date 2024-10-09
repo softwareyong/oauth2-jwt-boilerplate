f01f3980c9b2717e00dfa4bcfb7b02eb73e0094b
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-10-09 19:56:34 +0900

    feat: 프로젝트 초기 생성
    프로젝트 생성

---
+++ oauth2_jwt/src/test/java/com/oauth2_jwt/Oauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
